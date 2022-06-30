2c5ebe9f85d780ab357e588ad72401d76390b72d
meettan <meettan@gmail.com>
2022-06-30 18:07:25 +0530

    30062022

--- application/views/report/jrnl_prn/adv_jrnl.php
+++ application/views/report/jrnl_prn/adv_jrnl.php
@@ -180,7 +180,7 @@ function printDiv() {
 		$digits_1 = strlen($no);
 		$i = 0;
 		$str = array();
-		$words = array('0' => '', '1' => 'one', '2' => 'two',
+		$words = array('0' => 'zero', '1' => 'one', '2' => 'two',
 		 '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
 		 '7' => 'seven', '8' => 'eight', '9' => 'nine',
 		 '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
