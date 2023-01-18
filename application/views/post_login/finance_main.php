8b2c7147a087c8493111710e107fd8909a82cd0d
meettan <meettan@synergicsoftek.com>
2023-01-18 16:10:32 +0530

    18012023

--- application/views/post_login/finance_main.php
+++ application/views/post_login/finance_main.php
@@ -253,7 +253,7 @@
                         <a href="<?php echo site_url('trailbal'); ?>">Trial balance </a>
                         <?php if ($this->session->userdata['loggedin']['branch_id'] == 342) { ?>
                             <a href="<?php echo site_url('consolidated-trailbal'); ?>">Consolidated Trial</a>
-                            <a href="<?php echo site_url('consolidated-trailbal-subgroup'); ?>">Sub Group Wise Consolidated Trial</a>
+                            <a href="<?php echo site_url('consolidated-trailbal-subgroup'); ?>">Sub Group Wise Acc details</a>
                         <?php } ?>
                         <!-- <div class="dropdown-content"> -->
                         <!-- <div class="sub-dropdown">
