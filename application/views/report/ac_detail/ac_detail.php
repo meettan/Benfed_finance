9a59a6cf187e5198764ae40b3d377aa83b287375
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-04-19 13:06:09 +0530

    chnage

--- application/views/report/ac_detail/ac_detail.php
+++ application/views/report/ac_detail/ac_detail.php
@@ -83,8 +83,8 @@
                         <th rowspan='2'>Voucher Type</th>
                         <th rowspan='2'>Narration</th>
                         <th rowspan='2'>Voucher No</th>
-                        <th rowspan='2'>Transition No</th>
-                        <th colspan='2'>Trancation</th>
+                        <th rowspan='2'>Ref. No.</th>
+                        <th colspan='2'>Transaction</th>
                     </tr>
                     <tr>
                         <th>Debit</th>
@@ -173,7 +173,7 @@
             
         <?php }?> 
         
-        <tfoot>
+       
             <tr>
             <th>Total</th>
                 <th></th>
@@ -264,7 +264,7 @@
             </th>
 				
             </tr>
-        </tfoot>
+       
             
         </tbody>
         
