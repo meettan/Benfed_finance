e0e6abe0203b2a6d8f56713439a748b970911fbd
Suman Mitra <sumanmitra0096@gmail.com>
2025-09-05 15:00:12 +0530

    chnage

--- application/views/report/ac_detail/ac_detail.php
+++ application/views/report/ac_detail/ac_detail.php
@@ -227,32 +227,33 @@
         window.open("<?php echo site_url('report/voucher_dtls?voucher_id=');?>"+vid, '_blank');
     }
 </script>
-<!-- DataTables & Buttons -->
-<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" />
-<link href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css" rel="stylesheet" />
+<!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
 
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
-<!-- html2pdf.js for PDF export -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 
 <script>
-	$('#example').dataTable({
-		destroy: true,
-		searching: false,
-		ordering: false,
-		paging: false,
-		scrollX: false,
-		dom: 'Bfrtip',
-		buttons: [
-			{
-				extend: 'excelHtml5',
-				title: 'Account Details',
-				text: 'Export to Excel'
-			}
-		]
-	});
+$(document).ready(function() {
+    $('#example').DataTable({
+        dom: 'Bfrtip',
+        paging: false,
+        searching: false,
+        ordering: false,
+        buttons: [
+            {
+                extend: 'excelHtml5',
+                text: 'Export to Excel',
+                title: 'Account Details'
+            }
+        ]
+    });
+});
 </script>
