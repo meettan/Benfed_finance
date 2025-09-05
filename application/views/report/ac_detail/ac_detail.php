6c0006c86eb9b32e785631ef0458ae949163d067
Suman Mitra <sumanmitra0096@gmail.com>
2025-09-05 14:57:30 +0530

    chnage

--- application/views/report/ac_detail/ac_detail.php
+++ application/views/report/ac_detail/ac_detail.php
@@ -227,3 +227,32 @@
         window.open("<?php echo site_url('report/voucher_dtls?voucher_id=');?>"+vid, '_blank');
     }
 </script>
+<!-- DataTables & Buttons -->
+<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css" rel="stylesheet" />
+
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+
+<!-- html2pdf.js for PDF export -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+
+<script>
+	$('#example').dataTable({
+		destroy: true,
+		searching: false,
+		ordering: false,
+		paging: false,
+		scrollX: false,
+		dom: 'Bfrtip',
+		buttons: [
+			{
+				extend: 'excelHtml5',
+				title: 'Account Details',
+				text: 'Export to Excel'
+			}
+		]
+	});
+</script>
