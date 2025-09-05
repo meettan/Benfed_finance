69a150900b013c72a21ee3e2768f61815bbac783
Suman Mitra <sumanmitra0096@gmail.com>
2025-09-05 15:19:46 +0530

    chnage

--- application/views/report/ac_detail/ac_detail.php
+++ application/views/report/ac_detail/ac_detail.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
 
 <style>
     table {
