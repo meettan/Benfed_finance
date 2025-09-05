82d429df20338ae82f1522daa25b1c55e46a994f
Suman Mitra <sumanmitra0096@gmail.com>
2025-09-05 11:38:36 +0530

    change

--- application/views/report/ac_detail/ac_detail.php
+++ application/views/report/ac_detail/ac_detail.php
@@ -1,94 +1,34 @@
-
-
-
 <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
 
-   
 <style>
-	table {
-		border-collapse: collapse;
-	}
+    table {
+        border-collapse: collapse;
+    }
+    table, td, th {
+        border: 1px solid #dddddd;
+        padding: 6px;
+        font-size: 14px;
+    }
+    th {
+        text-align: center;
+    }
+    tr:hover {
+        background-color: #f5f5f5;
+    }
 
-	table,
-	td,
-	th {
-		border: 1px solid #dddddd;
-		padding: 6px;
-		font-size: 14px;
-	}
-
-	th {
-		text-align: center;
-	}
-
-	tr:hover {
-		background-color: #f5f5f5;
-	}
-
-	/* Hide DataTables buttons only in print */
-	@media print {
-		.dt-buttons,
-		.print-btn,
-		.pdf-btn {
-			display: none !important;
-			visibility: hidden !important;
-		}
-	}
+    /* Hide DataTables buttons only in print */
+    @media print {
+        .dt-buttons,
+        .print-btn,
+        .pdf-btn {
+            display: none !important;
+            visibility: hidden !important;
+        }
+    }
 </style>
 
-<script>
-	function printDiv() {
-		var divToPrint = document.getElementById('divToPrint');
-		var WindowObject = window.open('', 'Print-Window');
-		WindowObject.document.open();
-		WindowObject.document.writeln('<!DOCTYPE html>');
-		WindowObject.document.writeln('<html><head><title>Account Details</title><style type="text/css">');
-
-		// CSS inside print window
-		WindowObject.document.writeln(
-			'table { border-collapse: collapse; }' +
-			'table, td, th { border: 1px solid #dddddd; padding: 6px; font-size: 14px; }' +
-			'th { text-align: center; }' +
-			'.dt-buttons, .print-btn, .pdf-btn { display: none !important; visibility: hidden !important; }' +
-			'.center { text-align: center; }' +
-			'body { padding: 0; margin:0; }' +
-			'.billPrintWrapper { padding: 15px; color: #333; }'
-		);
-
-		WindowObject.document.writeln('</style></head><body onload="window.print()">');
-		WindowObject.document.writeln(divToPrint.innerHTML);
-		WindowObject.document.writeln('</body></html>');
-		WindowObject.document.close();
-
-		setTimeout(function () {
-			WindowObject.close();
-		}, 10);
-	}
-
-	// Save as PDF (html2pdf.js)
-	function savePDF() {
-		var element = document.getElementById('divToPrint');
-		// Temporarily hide Excel button for PDF
-		var excelBtn = document.querySelector('.dt-buttons');
-		if (excelBtn) excelBtn.style.display = "none";
-
-		var opt = {
-			margin: 0.5,
-			filename: 'Account Details.pdf',
-			image: { type: 'jpeg', quality: 0.98 },
-			html2canvas: { scale: 2 },
-			jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait' }
-		};
-		html2pdf().set(opt).from(element).save().then(() => {
-			// Show back Excel button after PDF
-			if (excelBtn) excelBtn.style.display = "block";
-		});
-	}
-</script>
-
 <div class="wraper">
-
     <div class="col-lg-12 container contant-wraper">
         <div id="divToPrint">
 
@@ -102,8 +42,9 @@
                     <?php echo $this->session->userdata['loggedin']['branch_name']; ?></h5>
             </div>
             <br>
-            <table id="example" class="display"  style=" width: 100%">
-            <thead>
+
+            <table id="example" class="display" style="width: 100%">
+                <thead>
                     <tr>
                         <th rowspan='2' style="width:90px !important">Date</th>
                         <th rowspan='2'>Particulars</th>
@@ -118,208 +59,75 @@
                         <th>Debit</th>
                         <th>Credit</th>
                     </tr>
-
                 </thead>
-        <tbody>
-            <?php 
-            if($accdetail){
-            $i = 1;
-            $total = 0.00;$ope_bal = 0.00;$cls_bal = 0.00;$opdr=0.00;$opcr=0.00;
-            $tot_debit = 0.00;$tot_cre =0.00;
-            $val =0;
-            $type =0;
-            if($opebalcal){
-										$opdr =$opebalcal->dr_amt;
-										$opcr =$opebalcal->cr_amt;
-										if($opebalcal->type == 1 ){
-									       $ope_bal = $ope_bal+$opcr-$opdr;
-											//echo $ope_bal ;
-											//$ope_bal = $opcr + $opdr;
-										}else if($opebalcal->type == 2 ){
-										$ope_bal = $ope_bal+$opdr-$opcr;
-											//$ope_bal = $opcr + $opdr;
-										}else if( $opebalcal->type ==3|| $opebalcal->type == 4){
-                                            $ope_bal=0.00;
-                                        }
-									} ?>
-            <tr>
-                <td></td>
-                <td>Opening Balance	</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td><?php  if($opebalcal){ if($opebalcal->trans_flag=='DR'){
-                                            echo abs($ope_bal);
-                                            }else{
-                                                echo "";
-                                            }
-                                        }else{
-                                            echo $ope_bal;
-                                        }
-                    ?>
-                </td>
-                <td><?php   if($opebalcal){ if($opebalcal->trans_flag=='CR'){
-                                            echo abs($ope_bal);
-                                            }else{
-                                                echo "";
-                                            }
-                                        }else{
-                                            echo $ope_bal;
-                                        }
-                    ?></td>
-            </tr>
-
-            <?php   foreach($trail_balnce as $tb){
-            $type = $tb->type;
-            ?>
-            <tr class="rep">
-
-            <td><?php echo date('d-m-Y',strtotime($tb->voucher_date)); ?></td>
-            <td><?php echo $tb->ac_name; ?></td>
-            <td><?php if($tb->voucher_mode == 'C'){echo 'Cash'; } 
-            elseif($tb->voucher_mode == 'J'){ echo 'Journal'; }
-            elseif($tb->voucher_mode == 'B'){ echo 'Bank'; }
-           
-           ?>
-            </td>
-            <td style="width:30%;word-wrap: break-word"><?php echo $tb->remarks; ?></td>
-         
-  			<td><a href="javascript:void(0)" onclick="voucherdtls('<?php echo $tb->voucher_id; ?>')"><?php echo $tb->voucher_id; ?></a></td>
-
-            <td><?php if(!empty($tb->trans_no)){echo $tb->trans_no;} ?></td>
-            <td><?php foreach($inv_detail as $inv) {
-                if(!empty($tb->trans_no)){
-                    if($tb->trans_no == $inv->ro_no){ echo $inv->invoice_no; }
-                }
-                
-            } ?></td>
-            <td align="right" style="width:90px !important"><?php echo number_format( $tb->dr_amt,2, '.', ''); $tot_debit +=$tb->dr_amt; ?></td>
-            <td align="right"><?php echo  number_format($tb->cr_amt,2, '.', ''); $tot_cre +=$tb->cr_amt;?></td>
-
-            </tr>
-            <?php  
-            }
-            ?>
-
-
-        <?php }?> 
-        
-       
-            <tr>
-            <th>Total</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th align="right"><?=$tot_debit?></th>
-                <th align="right"><?=$tot_cre?></th>
-            </tr>
-            <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th>Closing Balance</th>
-            <th></th>
-            <th></th>            
-            <th></th>
-               <th align="right">
-                 <?php 
-                 if($opebalcal){
-                    if($opebalcal->trans_flag=='DR'&& abs($ope_bal)+$tot_debit>$tot_cre ){ 
-                            $clBl=abs($ope_bal)+$tot_debit-$tot_cre;
-                            //echo abs($clBl);
-                            //echo number_format((float)abs($clBl), 2, '.', '');
-                            echo number_format(round(abs($clBl), 2),2, '.', '');
-                    }else{
-                        echo '';
-                    }
-                 }else{
-
-                    if(abs($ope_bal)+$tot_debit>$tot_cre ){ 
-                         $clBl=abs($ope_bal)+$tot_debit-$tot_cre;
-                         //echo abs($clBl);
-                        // echo number_format((float)abs($clBl), 2, '.', '');
-                        echo number_format(round(abs($clBl), 2),2, '.', '');
-                      }
-
-                 }
-
-            ?>
-            <?php 
-                if($opebalcal){
-                if($opebalcal->trans_flag=='CR' && abs($ope_bal)+$tot_cre <$tot_debit && abs($ope_bal) + $tot_cre - $tot_debit <0)
-                 { 
-                    $clBl=abs($ope_bal)+$tot_cre-$tot_debit;
-                
-                   // echo number_format((float)abs($clBl), 2, '.', '');
-                    echo number_format(round(abs($clBl), 2),2, '.', '');
-                 }else{
-                     echo '';
-                 }
-                }
-            ?>
-            
-            </th>
-            <th align="right">
-          
-            <?php   if($opebalcal){
-                        if($opebalcal->trans_flag=='CR' && abs($ope_bal)+$tot_cre>$tot_debit){
-                            $clBl=(abs($ope_bal)+$tot_cre) - $tot_debit;
-                            echo number_format(round(abs($clBl),2),2, '.', '');
-                        }else{
-                            echo '';
-                        }
-                    }
-                    // else{
-                    //     if(abs($ope_bal)+$tot_cre>$tot_debit){
-                    //     $clBl=(abs($ope_bal)+$tot_cre) - $tot_debit;
-                    //     echo round(abs($clBl),2);
-                    //     }
-                    // }
-			?>
-
-			<?php 
-                   if($opebalcal){
-                        if($opebalcal->trans_flag=='DR' && abs($ope_bal)+$tot_debit<$tot_cre && abs($ope_bal) + $tot_debit - $tot_cre <0){ 
-                
-                            $clBl=(abs($ope_bal)+$tot_debit)-$tot_cre;
-                            echo number_format(round(abs($clBl),2),2, '.', '');
-                        }else{
-                            echo '';
-                        }
-                   }else{
-
-                            if(abs($ope_bal)+$tot_debit<$tot_cre && abs($ope_bal) + $tot_debit - $tot_cre <0){ 
-                        
-                                $clBl=(abs($ope_bal)+$tot_debit)-$tot_cre;
-                                echo number_format(round(abs($clBl),2),2, '.', '');
+                <tbody>
+                    <?php 
+                    if($accdetail){
+                        $i = 1;
+                        $total = 0.00;$ope_bal = 0.00;$cls_bal = 0.00;$opdr=0.00;$opcr=0.00;
+                        $tot_debit = 0.00;$tot_cre =0.00;
+                        $val =0;
+                        $type =0;
+                        if($opebalcal){
+                            $opdr =$opebalcal->dr_amt;
+                            $opcr =$opebalcal->cr_amt;
+                            if($opebalcal->type == 1 ){
+                               $ope_bal = $ope_bal+$opcr-$opdr;
+                            } else if($opebalcal->type == 2 ){
+                               $ope_bal = $ope_bal+$opdr-$opcr;
+                            } else if( $opebalcal->type ==3|| $opebalcal->type == 4){
+                                $ope_bal=0.00;
                             }
-                   }
-            ?>
-            </th>
-				
-            </tr>
-       
-            
-        </tbody>
-        
-    </table>
+                        } ?>
+                        <tr>
+                            <td></td>
+                            <td>Opening Balance</td>
+                            <td></td><td></td><td></td><td></td><td></td>
+                            <td><?php if($opebalcal){ if($opebalcal->trans_flag=='DR'){ echo abs($ope_bal); } }?></td>
+                            <td><?php if($opebalcal){ if($opebalcal->trans_flag=='CR'){ echo abs($ope_bal); } }?></td>
+                        </tr>
 
-    
-            
+                        <?php foreach($trail_balnce as $tb){
+                            $type = $tb->type;
+                        ?>
+                        <tr class="rep">
+                            <td><?php echo date('d-m-Y',strtotime($tb->voucher_date)); ?></td>
+                            <td><?php echo $tb->ac_name; ?></td>
+                            <td><?php if($tb->voucher_mode == 'C'){echo 'Cash'; } 
+                                elseif($tb->voucher_mode == 'J'){ echo 'Journal'; }
+                                elseif($tb->voucher_mode == 'B'){ echo 'Bank'; }
+                            ?></td>
+                            <td style="width:30%;word-wrap: break-word"><?php echo $tb->remarks; ?></td>
+                            <td><a href="javascript:void(0)" onclick="voucherdtls('<?php echo $tb->voucher_id; ?>')"><?php echo $tb->voucher_id; ?></a></td>
+                            <td><?php if(!empty($tb->trans_no)){echo $tb->trans_no;} ?></td>
+                            <td><?php foreach($inv_detail as $inv) {
+                                if(!empty($tb->trans_no)){
+                                    if($tb->trans_no == $inv->ro_no){ echo $inv->invoice_no; }
+                                }
+                            } ?></td>
+                            <td align="right" style="width:90px !important"><?php echo number_format( $tb->dr_amt,2, '.', ''); $tot_debit +=$tb->dr_amt; ?></td>
+                            <td align="right"><?php echo  number_format($tb->cr_amt,2, '.', ''); $tot_cre +=$tb->cr_amt;?></td>
+                        </tr>
+                        <?php } ?>
+
+                        <tr>
+                            <th>Total</th>
+                            <th colspan="6"></th>
+                            <th align="right"><?=$tot_debit?></th>
+                            <th align="right"><?=$tot_cre?></th>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
         </div>
 
-        <div style="text-align: center;">
+        <div style="text-align: center; margin-top:15px;">
             <button class="btn btn-primary" type="button" onclick="printDiv();">Print</button>
             <button class="btn btn-danger pdf-btn" type="button" onclick="savePDF();">Save as PDF</button>
         </div>
     </div>
 </div>
-
 
 <!-- DataTables & Buttons -->
 <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" />
@@ -330,35 +138,81 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
 
-<!-- html2pdf.js for PDF export -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+<!-- jsPDF + jsPDF-AutoTable -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.27/jspdf.plugin.autotable.min.js"></script>
 
 <script>
-   $('#example').dataTable({
-    destroy: true,
-   searching: false,ordering: false,paging: false,
+    // Initialize DataTable
+    $('#example').dataTable({
+        destroy: true,
+        searching: false,
+        ordering: false,
+        paging: false,
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'excelHtml5',
+                title: 'Accounts Details',
+                text: 'Export to excel'
+            }
+        ]
+    });
 
-dom: 'Bfrtip',
-buttons: [
-   {
-extend: 'excelHtml5',
-title: 'Accounts Details',
-text: 'Export to excel'
+    // Print function
+    function printDiv() {
+        var divToPrint = document.getElementById('divToPrint');
+        var WindowObject = window.open('', 'Print-Window');
+        WindowObject.document.open();
+        WindowObject.document.writeln('<!DOCTYPE html>');
+        WindowObject.document.writeln('<html><head><title>Account Details</title><style>');
+        WindowObject.document.writeln(
+            'table { border-collapse: collapse; }'+
+            'table, td, th { border: 1px solid #dddddd; padding: 6px; font-size: 14px; }'+
+            'th { text-align: center; }'+
+            '.dt-buttons, .print-btn, .pdf-btn { display: none !important; visibility: hidden !important; }'+
+            'body { padding:0; margin:0; }'
+        );
+        WindowObject.document.writeln('</style></head><body onload="window.print()">');
+        WindowObject.document.writeln(divToPrint.innerHTML);
+        WindowObject.document.writeln('</body></html>');
+        WindowObject.document.close();
+    }
 
-   }
-]
-   });
+    // PDF generation using jsPDF + AutoTable
+    function savePDF() {
+        const { jsPDF } = window.jspdf;
+        var doc = new jsPDF('p', 'pt', 'a4');
+        doc.setFontSize(10);
+        doc.text("THE WEST BENGAL STATE CO.OP.MARKETING FEDERATION LTD.", 40, 40);
+        doc.text("HEAD OFFICE: SOUTHEND CONCLAVE, 3RD FLOOR, 1582 RAJDANGA MAIN ROAD, KOLKATA-700107.", 40, 55);
+        doc.text("Ledger Name: <?=$accdetail->ac_name?>", 40, 70);
+        doc.text("Ledger Code: <?=$accdetail->benfed_ac_code?>", 40, 85);
+        doc.text("Account Detail: <?php echo $_SESSION['date']; ?>", 40, 100);
+        doc.text("District: <?php echo $this->session->userdata['loggedin']['branch_name']; ?>", 40, 115);
 
+        // AutoTable for table
+        doc.autoTable({
+            html: '#example',
+            startY: 130,
+            theme: 'grid',
+            styles: { fontSize: 8 },
+            headStyles: { fillColor: [52, 73, 94], textColor: 255 },
+            footStyles: { fillColor: [52, 73, 94], textColor: 255 },
+            margin: { left: 40, right: 40 },
+            didDrawPage: function (data) {
+                // Optional: Add page number
+                var pageCount = doc.getNumberOfPages();
+                doc.setFontSize(8);
+                doc.text('Page ' + pageCount, data.settings.margin.left, doc.internal.pageSize.height - 10);
+            }
+        });
+
+        doc.save('Account_Details.pdf');
+    }
+
+    // Voucher details popup
+    function voucherdtls(vid){
+        window.open("<?php echo site_url('report/voucher_dtls?voucher_id=');?>"+vid, '_blank');
+    }
 </script>
-<script>
-function voucherdtls(vid){
-
-window.open("<?php echo site_url('report/voucher_dtls?voucher_id=');?>"+vid, '_blank');
-
-}
-</script>
-
-
-
-
-
