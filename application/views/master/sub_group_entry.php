d9da14479f5eb41d885ed3030ab8ffac007b60c6
Suman Mitra <sumanmitra0096@gmail.com>
2025-11-13 14:47:36 +0530

    chnage

--- application/views/master/sub_group_entry.php
+++ application/views/master/sub_group_entry.php
@@ -56,7 +56,10 @@
 				<div class="col-sm-10">
 
 				<input type="submit" class="btn btn-info" value="Save" />
-
+				<a href="<?php echo site_url("subgroup"); ?>" 
+                class="btn btn-danger" 
+                style="width: 100px; margin-left:10px;">Back
+            </a>
 				</div>
 
 			</div>
